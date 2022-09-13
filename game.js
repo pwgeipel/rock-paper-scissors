7b8dfef7fd5940ac07e5d625e9639ea5cd4ba383
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-09-12 19:37:10 -0500

    pseudo-code

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
-console.log("yo!")
+// define array for choices [r, p, s]
+// define var for computerChoice
+// define var for humanChoice
+// define var for wins
+// define var for losses
+// define var for ties
+
+// promt humanChoice
+
+// randomly choose computerChoice
+
+// compare choices
+    // if humanChoice is p and computerChoice is r
+
+    
+// display (alert) results (won, tied, lost round)
+
+// show stats (number of wins, losses, ties)
+
+// play again?
+    // restart game
+// end game
